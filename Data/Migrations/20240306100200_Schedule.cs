8be51b382a4687f265bfb85faee4e42c4fc0e70b
Navid <navidghate@hotmail.com>
2024-03-06 13:54:58 +0100

    added db migrations, test on website to add, edit and delete

---
+++ Data/Migrations/20240306100200_Schedule.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Schedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<string>(type: "TEXT", nullable: false),
+                    Therapy = table.Column<string>(type: "TEXT", nullable: false),
+                    Length = table.Column<int>(type: "INTEGER", nullable: false),
+                    BookedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    Booked = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schedules");
+        }
+    }
+}
